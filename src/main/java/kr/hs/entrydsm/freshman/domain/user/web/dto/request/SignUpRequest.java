b34eb5e1ecbb5e610a0a138c0f16f4db76373da0
정대현 <eogus_2513@dsm.hs.kr>
2022-07-16 21:14:43 +0900

    ⚡️ :: @Max(10) 추가

--- src/main/java/kr/hs/entrydsm/freshman/domain/user/web/dto/request/SignUpRequest.java
+++ src/main/java/kr/hs/entrydsm/freshman/domain/user/web/dto/request/SignUpRequest.java
@@ -3,6 +3,7 @@ package kr.hs.entrydsm.freshman.domain.user.web.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -15,9 +16,10 @@ public class SignUpRequest {
 
     @NotBlank
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,16}$",
-    message = "password는 대문자[A-Z], 소문자[a-z], 숫자[0-9], 특수문자[!, @, #, $, %, ^, &, +, =]가 최소 1개씩 포함되어야 합니다.")
+            message = "password는 대문자[A-Z], 소문자[a-z], 숫자[0-9], 특수문자[!, @, #, $, %, ^, &, +, =]가 최소 1개씩 포함되어야 합니다.")
     private String password;
 
     @NotBlank
+    @Max(10)
     private String name;
 }
