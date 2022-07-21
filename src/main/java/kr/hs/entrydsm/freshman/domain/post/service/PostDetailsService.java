2eeb08902175a43764a3d241766c4b0d4b61cea8
정대현 <eogus_2513@dsm.hs.kr>
2022-07-21 22:08:38 +0900

    :bug: :: 500 에러 해결 - anonymousUser

--- src/main/java/kr/hs/entrydsm/freshman/domain/post/service/PostDetailsService.java
+++ src/main/java/kr/hs/entrydsm/freshman/domain/post/service/PostDetailsService.java
@@ -14,16 +14,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PostDetailsService {
 
+    private final static String ANONYMOUS_USER = "anonymousUser";
     private final CurrentUserFacade currentUserFacade;
     private final PostRepository postRepository;
 
     @Transactional(readOnly = true)
     public PostDetailsResponse execute(Long postId) {
-        User user = currentUserFacade.getCurrentUser();
-
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> PostNotFoundException.EXCEPTION);
 
+        if (ANONYMOUS_USER.equals(currentUserFacade.getCurrentUserAccountId())) {
+            return buildPostDetails(post, User.builder().build());
+        } else {
+            User user = currentUserFacade.getCurrentUser();
+            return buildPostDetails(post, user);
+        }
+    }
+
+    private PostDetailsResponse buildPostDetails(Post post, User user) {
         return PostDetailsResponse.builder()
                 .name(post.getUser().getName())
                 .title(post.getTitle())
